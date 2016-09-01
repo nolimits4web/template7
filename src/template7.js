874d06d31ddc868a37a124bb2598759052107a06
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-09-01 18:26:43 +0300

    Better support for double/single quotes mixing

--- src/template7.js
+++ src/template7.js
@@ -9,18 +9,30 @@ window.Template7 = (function () {
     function isFunction(func) {
         return typeof func === 'function';
     }
+    function _escape(string) {
+        return typeof window !== 'undefined' && window.escape ? window.escape(string) : string
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;');
+    }
     var cache = {};
+    var quoteSingleRexExp = new RegExp('\'', 'g');
+    var quoteDoubleRexExp = new RegExp('"', 'g');
     function helperToSlices(string) {
         var helperParts = string.replace(/[{}#}]/g, '').split(' ');
         var slices = [];
         var shiftIndex, i, j;
         for (i = 0; i < helperParts.length; i++) {
             var part = helperParts[i];
+            var blockQuoteRegExp, openingQuote;
             if (i === 0) slices.push(part);
             else {
-                if (part.indexOf('"') === 0) {
+                if (part.indexOf('"') === 0 || part.indexOf('\'') === 0) {
+                    blockQuoteRegExp = part.indexOf('"') === 0 ? quoteDoubleRexExp : quoteSingleRexExp;
+                    openingQuote = part.indexOf('"') === 0 ? '"' : '\'';
                     // Plain String
-                    if (part.match(/"/g).length === 2) {
+                    if (part.match(blockQuoteRegExp).length === 2) {
                         // One word string
                         slices.push(part);
                     }
@@ -29,7 +41,7 @@ window.Template7 = (function () {
                         shiftIndex = 0;
                         for (j = i + 1; j < helperParts.length; j++) {
                             part += ' ' + helperParts[j];
-                            if (helperParts[j].indexOf('"') >= 0) {
+                            if (helperParts[j].indexOf(openingQuote) >= 0) {
                                 shiftIndex = j;
                                 slices.push(part);
                                 break;
@@ -44,18 +56,18 @@ window.Template7 = (function () {
                         var hashParts = part.split('=');
                         var hashName = hashParts[0];
                         var hashContent = hashParts[1];
-                        if (hashContent.match(/"/g).length !== 2) {
+                        if (hashContent.match(blockQuoteRegExp).length !== 2) {
                             shiftIndex = 0;
                             for (j = i + 1; j < helperParts.length; j++) {
                                 hashContent += ' ' + helperParts[j];
-                                if (helperParts[j].indexOf('"') >= 0) {
+                                if (helperParts[j].indexOf(openingQuote) >= 0) {
                                     shiftIndex = j;
                                     break;
                                 }
                             }
                             if (shiftIndex) i = shiftIndex;
                         }
-                        var hash = [hashName, hashContent.replace(/"/g,'')];
+                        var hash = [hashName, hashContent.replace(blockQuoteRegExp,'')];
                         slices.push(hash);
                     }
                     else {
@@ -108,7 +120,7 @@ window.Template7 = (function () {
                         helperContext.push(slice);
                     }
                 }
-                
+
                 if (block.indexOf('{#') >= 0) {
                     // Condition/Helper
                     var helperStartIndex = i;
@@ -178,10 +190,10 @@ window.Template7 = (function () {
         }
         return blocks;
     }
-    var Template7 = function (template) {
+    var Template7 = function (template, options) {
         var t = this;
         t.template = template;
-        
+
         function getCompileFn(block, depth) {
             if (block.content) return compile(block.content, depth);
             else return function () {return ''; };
@@ -229,7 +241,7 @@ window.Template7 = (function () {
                             variable = part.replace('this', ctx);
                         }
                         else {
-                            variable += '.' + part;       
+                            variable += '.' + part;
                         }
                     }
                 }
@@ -292,9 +304,9 @@ window.Template7 = (function () {
                 if (block.type === 'helper') {
                     if (block.helperName in t.helpers) {
                         compiledArguments = getCompiledArguments(block.contextName, ctx);
-                        
+
                         resultString += 'r += (Template7.helpers.' + block.helperName + ').call(' + ctx + ', ' + (compiledArguments && (compiledArguments + ', ')) +'{hash:' + JSON.stringify(block.hash) + ', data: data || {}, fn: ' + getCompileFn(block, depth + 1) + ', inverse: ' + getCompileInverse(block, depth + 1) + ', root: root});';
-                        
+
                     }
                     else {
                         if (block.contextName.length > 0) {
@@ -330,7 +342,7 @@ window.Template7 = (function () {
                 var p = Template7.prototype.partials[partialName];
                 if (!p || (p && !p.template)) return '';
                 if (!p.compiled) {
-                    p.compiled = t7.compile(p.template);
+                    p.compiled = new Template7(p.template).compile();
                 }
                 var ctx = this;
                 for (var hashName in options.hash) {
@@ -342,11 +354,7 @@ window.Template7 = (function () {
                 if (typeof context !== 'string') {
                     throw new Error('Template7: Passed context to "escape" helper should be a string');
                 }
-                return context
-                        .replace(/&/g, '&amp;')
-                        .replace(/</g, '&lt;')
-                        .replace(/>/g, '&gt;')
-                        .replace(/"/g, '&quot;');
+                return _escape(context);
             },
             'if': function (context, options) {
                 if (isFunction(context)) { context = context.call(this); }
@@ -420,7 +428,7 @@ window.Template7 = (function () {
                     return options.fn(this, options.data);
                 }
                 else {
-                    return options.inverse(this, options.data);   
+                    return options.inverse(this, options.data);
                 }
             }
         }
@@ -438,7 +446,7 @@ window.Template7 = (function () {
         Template7.prototype.helpers[name] = fn;
     };
     t7.unregisterHelper = function (name) {
-        Template7.prototype.helpers[name] = undefined;  
+        Template7.prototype.helpers[name] = undefined;
         delete Template7.prototype.helpers[name];
     };
     t7.registerPartial = function (name, template) {
@@ -450,12 +458,11 @@ window.Template7 = (function () {
             delete Template7.prototype.partials[name];
         }
     };
-    
     t7.compile = function (template, options) {
         var instance = new Template7(template, options);
         return instance.compile();
     };
-    
+
     t7.options = Template7.prototype.options;
     t7.helpers = Template7.prototype.helpers;
     t7.partials = Template7.prototype.partials;
