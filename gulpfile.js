71d2f3dc53d7e2375000d7470fb2a451d3af8559
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-15 13:59:32 +0300

    Build with rollup
    Fixes #13
    Fixes #32

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@
     'use strict';
     var gulp = require('gulp'),
         connect = require('gulp-connect'),
+        eslint = require('gulp-eslint'),
         open = require('gulp-open'),
         rename = require('gulp-rename'),
         header = require('gulp-header'),
         path = require('path'),
         uglify = require('gulp-uglify'),
         sourcemaps = require('gulp-sourcemaps'),
-        jshint = require('gulp-jshint'),
-        stylish = require('jshint-stylish'),
+        rollup = require('rollup-stream'),
+        buble = require('rollup-plugin-buble'),
+        source = require('vinyl-source-stream'),
+        buffer = require('vinyl-buffer'),
         paths = {
             root: './',
             build: 'build/',
@@ -43,8 +46,6 @@
             }
         };
 
-    
-    
     gulp.task('build', function (cb) {
         gulp.src(paths.source + 'template7.js')
             .pipe(sourcemaps.init())
@@ -57,19 +58,58 @@
         cb();
     });
 
-    gulp.task('dist', function () {
-        gulp.src(paths.build + 'template7.js')
-            .pipe(gulp.dest(paths.dist))
-            .pipe(sourcemaps.init())
-            .pipe(uglify())
-            .pipe(header(t7.banner, { pkg : t7.pkg, date: t7.date }))
-            .pipe(rename(function(path) {
-                path.basename = t7.filename + '.min';
-            }))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest(paths.dist));
-        gulp.src(paths.build + 'template7.js.map')
-            .pipe(gulp.dest(paths.dist));
+    gulp.task('build', function (cb) {
+        rollup({
+            entry: './src/template7.js',
+            plugins: [buble()],
+            format: 'umd',
+            moduleName: 'Template7',
+            useStrict: true,
+            sourceMap: true
+        })
+        .pipe(source('template7.js', './src'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./build/'))
+        .on('end', function () {
+            cb();
+        });
+    });
+
+    gulp.task('dist', function (cb) {
+        rollup({
+            entry: './src/template7.js',
+            plugins: [buble()],
+            format: 'umd',
+            moduleName: 'Template7',
+            useStrict: true,
+            sourceMap: true
+        })
+        .pipe(source('template7.js', './src'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(header(t7.banner, {
+            pkg: t7.pkg,
+            date: t7.date
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/'))
+        .on('end', function () {
+            gulp.src('./dist/template7.js')
+                .pipe(sourcemaps.init())
+                .pipe(uglify())
+                .pipe(header(t7.banner, {
+                    pkg: t7.pkg,
+                    date: t7.date
+                }))
+                .pipe(rename('template7.min.js'))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('./dist/'))
+                .on('end', function () {
+                    cb();
+                });
+        });
     });
 
     gulp.task('watch', function () {
