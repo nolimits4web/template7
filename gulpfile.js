6a2036c0508c61bd9035648c9bc0592645d28354
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 16:31:13 +0300

    Code formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,152 +1,145 @@
-(function(){
-    'use strict';
-    var gulp = require('gulp'),
-        connect = require('gulp-connect'),
-        open = require('gulp-open'),
-        rename = require('gulp-rename'),
-        header = require('gulp-header'),
-        path = require('path'),
-        uglify = require('gulp-uglify'),
-        sourcemaps = require('gulp-sourcemaps'),
-        rollup = require('rollup-stream'),
-        buble = require('rollup-plugin-buble'),
-        source = require('vinyl-source-stream'),
-        buffer = require('vinyl-buffer'),
-        paths = {
-            root: './',
-            build: 'build/',
-            dist: 'dist/',
-            demo: 'demo/',
-            source: 'src/',
-        },
-        t7 = {
-            filename: 'template7',
-            pkg: require('./package.json'),
-            banner: [
-                '/**',
-                ' * Template7 <%= pkg.version %>',
-                ' * <%= pkg.description %>',
-                ' * ',
-                ' * <%= pkg.homepage %>',
-                ' * ',
-                ' * Copyright <%= date.year %>, <%= pkg.author %>',
-                ' * The iDangero.us',
-                ' * http://www.idangero.us/',
-                ' * ',
-                ' * Licensed under <%= pkg.license.join(" & ") %>',
-                ' * ',
-                ' * Released on: <%= date.month %> <%= date.day %>, <%= date.year %>',
-                ' */',
-                ''].join('\n'),
-            date: {
-                year: new Date().getFullYear(),
-                month: ('January February March April May June July August September October November December').split(' ')[new Date().getMonth()],
-                day: new Date().getDate()
-            }
-        };
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const open = require('gulp-open');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const rollup = require('rollup-stream');
+const buble = require('rollup-plugin-buble');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
-    // Build
-    gulp.task('build', function (cb) {
-        rollup({
-            entry: './src/template7.js',
-            plugins: [buble()],
-            format: 'umd',
-            moduleName: 'Template7',
-            useStrict: true,
-            sourceMap: true
-        })
-        .pipe(source('template7.js', './src'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./build/'))
-        .on('end', function () {
-            cb();
-        });
-    });
-    function umd(cb) {
-      rollup({
-          entry: './src/template7.js',
-          plugins: [buble()],
-          format: 'umd',
-          moduleName: 'Template7',
-          useStrict: true,
-          sourceMap: true
-      })
+const paths = {
+  root: './',
+  build: 'build/',
+  dist: 'dist/',
+  demo: 'demo/',
+  source: 'src/',
+};
+const t7 = {
+  filename: 'template7',
+  pkg: require('./package.json'),
+  banner: [
+    '/**',
+    ' * Template7 <%= pkg.version %>',
+    ' * <%= pkg.description %>',
+    ' * ',
+    ' * <%= pkg.homepage %>',
+    ' * ',
+    ' * Copyright <%= date.year %>, <%= pkg.author %>',
+    ' * The iDangero.us',
+    ' * http://www.idangero.us/',
+    ' * ',
+    ' * Licensed under <%= pkg.license.join(" & ") %>',
+    ' * ',
+    ' * Released on: <%= date.month %> <%= date.day %>, <%= date.year %>',
+    ' */',
+    ''].join('\n'),
+  date: {
+    year: new Date().getFullYear(),
+    month: ('January February March April May June July August September October November December').split(' ')[new Date().getMonth()],
+    day: new Date().getDate(),
+  },
+};
+
+  // Build
+gulp.task('build', (cb) => {
+  rollup({
+    input: './src/template7.js',
+    plugins: [buble()],
+    format: 'umd',
+    name: 'Template7',
+    strict: true,
+    sourcemap: true,
+  })
       .pipe(source('template7.js', './src'))
       .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(header(t7.banner, {
-          pkg: t7.pkg,
-          date: t7.date
-      }))
+      .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist/'))
-      .on('end', function () {
-          gulp.src('./dist/template7.js')
-              .pipe(sourcemaps.init())
-              .pipe(uglify())
-              .pipe(header(t7.banner, {
-                  pkg: t7.pkg,
-                  date: t7.date
-              }))
-              .pipe(rename('template7.min.js'))
-              .pipe(sourcemaps.write('./'))
-              .pipe(gulp.dest('./dist/'))
-              .on('end', function () {
-                  if (cb) cb();
-              });
+      .pipe(gulp.dest('./build/'))
+      .on('end', () => {
+        cb();
       });
-    }
-    function es(cb) {
-      rollup({
-        entry: './src/template7.js',
-        format: 'es',
-        moduleName: 'Template7',
-        useStrict: true,
-      })
-      .pipe(source('template7.js', './src'))
-      .pipe(buffer())
-      .pipe(header(t7.banner, {
-        pkg: t7.pkg,
-        date: t7.date
-      }))
-      .pipe(rename('template7.module.js'))
-      .pipe(gulp.dest('./dist/'))
-      .on('end', function () {
-        if (cb) cb();
-      });
-    }
-    // Dist
-    gulp.task('dist', function (cb) {
-      var cbs = 0;
-      umd(function () {
-        cbs += 1;
-        if (cbs === 2) cb();
-      });
-      es(function () {
-        cbs += 1;
-        if (cbs === 2) cb();
-      });
+});
+function umd(cb) {
+  rollup({
+    input: './src/template7.js',
+    plugins: [buble()],
+    format: 'umd',
+    name: 'Template7',
+    strict: true,
+    sourcemap: true,
+  })
+    .pipe(source('template7.js', './src'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(header(t7.banner, {
+      pkg: t7.pkg,
+      date: t7.date,
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'))
+    .on('end', () => {
+      gulp.src('./dist/template7.js')
+            .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(header(t7.banner, {
+              pkg: t7.pkg,
+              date: t7.date,
+            }))
+            .pipe(rename('template7.min.js'))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist/'))
+            .on('end', () => {
+              if (cb) cb();
+            });
     });
-
-    gulp.task('watch', function () {
-        gulp.watch('./src/*.js', [ 'build' ]);
+}
+function es(cb) {
+  rollup({
+    input: './src/template7.js',
+    format: 'es',
+    name: 'Template7',
+    strict: true,
+  })
+    .pipe(source('template7.js', './src'))
+    .pipe(buffer())
+    .pipe(header(t7.banner, {
+      pkg: t7.pkg,
+      date: t7.date,
+    }))
+    .pipe(rename('template7.module.js'))
+    .pipe(gulp.dest('./dist/'))
+    .on('end', () => {
+      if (cb) cb();
     });
+}
+// Dist
+gulp.task('dist', (cb) => {
+  let cbs = 0;
+  umd(() => {
+    cbs += 1;
+    if (cbs === 2) cb();
+  });
+  es(() => {
+    cbs += 1;
+    if (cbs === 2) cb();
+  });
+});
 
-    gulp.task('connect', function () {
-        return connect.server({
-            root: [ paths.root ],
-            livereload: true,
-            port:'3000'
-        });
-    });
+gulp.task('watch', () => {
+  gulp.watch('./src/*.js', ['build']);
+});
 
-    gulp.task('open', function () {
-        return gulp.src(paths.demo + 'index.html').pipe(open({ uri: 'http://localhost:3000/' + paths.demo + 'index.html'}));
-    });
+gulp.task('connect', () => connect.server({
+  root: [paths.root],
+  livereload: true,
+  port: '3000',
+}));
 
-    gulp.task('server', [ 'watch', 'connect', 'open' ]);
+gulp.task('open', () => gulp.src(`${paths.demo}index.html`).pipe(open({ uri: `http://localhost:3000/${paths.demo}index.html` })));
 
-    gulp.task('default', [ 'server' ]);
-})();
+gulp.task('server', ['watch', 'connect', 'open']);
+
+gulp.task('default', ['server']);
