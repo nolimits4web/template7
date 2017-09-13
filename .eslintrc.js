ccc4a8d3194034042bc8ac3810e8ffed0346c36f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 16:31:04 +0300

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "globals": {
-        "window": true
-    }
+  "extends": "airbnb-base",
+  "plugins": [
+      "import"
+  ],
+  "rules": {
+    "no-continue": ["off"],
+    "max-len": ["error", 1000, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+  },
+  "globals": {
+      "window": true
+  }
 };
