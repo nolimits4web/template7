0bc40866f910ff91ec269b2ffc339b3905556462
Jason Kleban <jkleban@outlook.com>
2018-08-30 20:29:22 -0400

    Oops, I don't think it should wrap in the named module.  Even though it isn't in the dist folder, the package.json lists it and I think it will understand association correctly.

--- template7.d.ts
+++ template7.d.ts
@@ -1,15 +1,13 @@
-declare module "Template7" {
-    interface Template7{
-        global?: any
-        templates?: any;
-        compile? (htmlString: string): any;
-        registerHelper? (name: string, helper: Function): any;
-        unregisterHelper? (name: string) : void;
-        registerPartial? (name: string, template: string) : void;
-        unregisterPartial? (name: string) : void;
-    }
-
-    const Template7 : Template7;
-
-    export default Template7
+interface Template7{
+    global?: any
+    templates?: any;
+    compile? (htmlString: string): any;
+    registerHelper? (name: string, helper: Function): any;
+    unregisterHelper? (name: string) : void;
+    registerPartial? (name: string, template: string) : void;
+    unregisterPartial? (name: string) : void;
 }
+
+declare const Template7 : Template7;
+
+export default Template7
